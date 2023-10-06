ee6e2a7cb1da5d3a9229d6061d8f014bf5084aaf
ian <ianokipla@gmail.com>
2023-10-06 12:00:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  // if ride <= 400 feet//
+  if(ride<='400')
+      return 'This one is on me!';
+
+  else if(ride > 2000 && ride <= 2500)
+    return 'I will gladly take your thirty bucks.';
+  else(ride>2500)
+  return'No can do.'; 
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  if( city!=='NYC')
+  return 'No go.';
+else return 'Ok, sounds good.'
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case"generous":
+    return'Thank you so much.';
+    case"not as generous":
+    return "Thank you.";
+    default:"thanks for everything"
+    return"Bye.";
+  }
+} 
+
+
+  
